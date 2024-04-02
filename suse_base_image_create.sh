fe22371ebf6cd62996c0a6623e5c2caabee7e509
Vladimir Zhurkin <i@icce.im>
2024-04-03 01:28:07 +0300

    Update suse_base_image_create.sh

--- suse_base_image_create.sh
+++ suse_base_image_create.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env bash
 #
 # Script Name: suse_base_image_create.sh
 # Description: This script installs necessary packages and configures repositories to create a base image of SUSE Linux for OCI (Open Container Initiative) compliant containers
@@ -40,16 +40,16 @@ zypp_cache_dir="$install_dir/var/cache/zypp"
 packages_to_install="aaa_base zypper ca-certificates ca-certificates-mozilla timezone"
 
 # Main repository URL variable
-s_repo="download.opensuse.org"
+s_repo="http://download.opensuse.org"
 
 # Array with repository information in the format NAME_REPOSITORY|LOCATION|ALIAS
 repositories=(
-    "Main Repository|distribution/leap/15.5/repo/oss/|repo-oss|99"
-    "Update Repository|update/leap/15.5/oss|repo-update|99"
-    "Repository Non-OSS|distribution/leap/15.5/repo/non-oss/|repo-non-oss|99"
-    "Update Repository Non-OSS|update/leap/15.5/non-oss/|repo-update-non-oss|99"
-    "Update repository of openSUSE Backports|update/leap/15.5/backports/|repo-backports-update|99"
-    "Update repository with updates from SUSE Linux Enterprise 15|update/leap/15.5/non-oss/|repo-sle-update|99"
+    "Main Repository|/distribution/leap/15.5/repo/oss/|repo-oss|99"
+    "Update Repository|/update/leap/15.5/oss|repo-update|99"
+    "Repository Non-OSS|/distribution/leap/15.5/repo/non-oss/|repo-non-oss|99"
+    "Update Repository Non-OSS|/update/leap/15.5/non-oss/|repo-update-non-oss|99"
+    "Update repository of openSUSE Backports|/update/leap/15.5/backports/|repo-backports-update|99"
+    "Update repository with updates from SUSE Linux Enterprise 15|/update/leap/15.5/non-oss/|repo-sle-update|99"
 )
 
 # Function to check the existence of the zypper command
